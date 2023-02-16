0298bc3c53a6969f00cf2aaf6d524f98005f9f3f
David Ward <David.Ward@standard.com>
2023-02-15 18:11:26 -0800

    More changes to board

--- projects/sudoku/scripts/drawBoard.js
+++ projects/sudoku/scripts/drawBoard.js
@@ -1,24 +1,21 @@
 export function drawInitialBoard() {
     const sudokuBoard = document.querySelector("#puzzle")
     const squares = 81
+    const darkSquare = [0,1,2,6,7,8,9,10,11,15,16,17,18,
+        19,20,24,25,26,30,31,32,39,40,41,48,49,50,54,55,
+        56,60,61,62,63,64,65,69,70,71,72,73,74,78,79,80];
 
     for (let i = 0; i < squares; i++) {
         const inputElement = document.createElement("input")
         
-        inputElement.setAttribute('type', 'number')
-        inputElement.setAttribute('min', '1')
-        inputElement.setAttribute('max', '9')
-
-        if (
-            ((i % 9 == 0 || i % 9 == 1 || i % 9 == 2) && i < 21) ||
-            ((i % 9 == 6 || i % 9 == 7 || i % 9 == 8) && i < 27) ||
-            ((i % 9 == 3 || i % 9 == 4 || i % 9 == 5) && (i > 27 && i < 53)) ||
-            ((i % 9 == 0 || i % 9 == 1 || i % 9 == 2) && i > 53) ||
-            ((i % 9 == 6 || i % 9 == 7 || i % 9 == 8) && i > 53)
-        ) {
-            inputElement.classList.add('odd-section')
+        inputElement.setAttribute("type", "number")
+        inputElement.setAttribute("oninput", "this.value=this.value.slice(0,this.maxLength)")
+        inputElement.setAttribute("maxlength", "1")
+       
+        if (darkSquare.includes(i)) {
+            inputElement.classList.add("odd-section")
         }
-        
         sudokuBoard.appendChild(inputElement)
     }
+    
 }
--- projects/sudoku/scripts/solver.js
+++ projects/sudoku/scripts/solver.js
@@ -1,15 +1,15 @@
 let board = []
 
 export function insertValues() {
-    const inputs = document.querySelectorAll('input')
+    const inputs = document.querySelectorAll("input")
     
     inputs.forEach((input) => {
         if(input.value) {
             board.push(parseInt(input.value))
-            input.classList.add('input-el') 
+            input.classList.add("input-el") 
         } else {
             board.push(0)
-            input.classList.add('empty-el')
+            input.classList.add("empty-el")
         }
     })
 }
