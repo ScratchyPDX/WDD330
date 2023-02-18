3cb9cfefc072ad61060e46dd14f904e2f43bced0
David Ward <David.Ward@standard.com>
2023-02-18 14:53:49 -0800

    Add solver/clear code

--- projects/sudoku/scripts/main.js
+++ projects/sudoku/scripts/main.js
@@ -1,3 +1,20 @@
 import { drawInitialBoard } from "./drawBoard.js";
+import { insertValues, solve, populateValues } from "./solver.js";
 
-drawInitialBoard();
+const solveButton = document.querySelector("#solve-button")
+const clearButton = document.querySelector("#clear-button")
+
+function main() {
+    drawInitialBoard()
+    solveButton.addEventListener('click', () => {
+        insertValues()
+        if(solve()) {
+            populateValues()
+        } else {
+            alert("Can't solve this puzzle!")
+        }
+    })
+    clearButton.addEventListener('click', () => window.location.reload(true))
+}
+
+main();
